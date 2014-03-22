e6119f07057f498613a996dfb001cc04e6462ae9
longpham.uit@gmail.com <longpham.uit@gmail.com@26accb79-be41-b226-8202-c70e911acedf>
2014-03-22 17:58:51 +0000

    + Complete read from plist

---
+++ trunk/TextEditor/Plist.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Data;
+
+namespace TextEditor
+{
+    public class Plist
+    {
+        private static DataTable mainTable;
+
+        public static DataTable Load(string file)
+        {
+            mainTable = new DataTable();
+
+            XDocument doc = XDocument.Load(file);
+            XElement plist = doc.Element("plist");
+            XElement dict = plist.Element("dict");
+
+            var dictElements = dict.Elements();
+            Parse(dictElements);
+
+            return mainTable;
+        }
+
+        private static void Parse(IEnumerable<XElement> elements)
+        {
+            //languages
+            int numberColumn = elements.Count() / 2;
+            Console.WriteLine("NumberColumn: " + numberColumn);
+
+            //get all language package, add to columns
+            for (int i = 0; i < numberColumn * 2; i += 2)
+            {
+                XElement key = elements.ElementAt(i);
+                mainTable.Columns.Add(key.Value);
+                Console.WriteLine("Add column: " + key.Value);
+            }
+
+            //get numberRow
+            XElement l = elements.ElementAt(1);
+            var lp = l.Elements();
+            int numberRow = lp.Count() / 2;
+            Console.WriteLine("NumberRow: " + numberRow);
+
+
+            //foreach all pack
+            for (int j = 0; j < numberRow * 2; j += 2)
+            {
+                string[] rows = new string[numberColumn];
+
+                //foreach each language package
+                for (int i = 0; i < numberColumn * 2; i += 2)
+                {
+                    XElement lang = elements.ElementAt(i + 1); //English -> Vietnamese -> more...
+                    var langpack = lang.Elements(); //dict English -> dict Vietnamese -> more...
+
+                    XElement text = langpack.ElementAt(j + 1);
+                    rows[i/2] = text.Value;
+                    Console.WriteLine(text.Value);
+                }
+
+                mainTable.Rows.Add(rows);
+            }
+        }
+    }
+}
--- trunk/TextEditor/frmMain.Designer.cs
+++ trunk/TextEditor/frmMain.Designer.cs
@@ -28,109 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.drgMain = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.English = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Vietnamese = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.itemHome = new System.Windows.Forms.ToolStripMenuItem();
             this.itemNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.itemSave = new System.Windows.Forms.ToolStripMenuItem();
             this.itemExport = new System.Windows.Forms.ToolStripMenuItem();
             this.itemPlist = new System.Windows.Forms.ToolStripMenuItem();
             this.itemJson = new System.Windows.Forms.ToolStripMenuItem();
             this.itemExit = new System.Windows.Forms.ToolStripMenuItem();
             this.itemAbout = new System.Windows.Forms.ToolStripMenuItem();
-            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.English = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Vietnamese = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemColumns = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.drgMain)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // drgMain
             // 
+            this.drgMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.drgMain.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.drgMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.drgMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Id,
             this.English,
             this.Vietnamese});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.drgMain.DefaultCellStyle = dataGridViewCellStyle2;
             this.drgMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.drgMain.Location = new System.Drawing.Point(0, 24);
+            this.drgMain.Location = new System.Drawing.Point(0, 28);
             this.drgMain.Name = "drgMain";
-            this.drgMain.Size = new System.Drawing.Size(851, 393);
+            this.drgMain.Size = new System.Drawing.Size(851, 389);
             this.drgMain.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.itemHome,
-            this.itemAbout});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(851, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuMain";
-            // 
-            // itemHome
-            // 
-            this.itemHome.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.itemNew,
-            this.itemSave,
-            this.itemExport,
-            this.itemExit});
-            this.itemHome.Name = "itemHome";
-            this.itemHome.Size = new System.Drawing.Size(52, 20);
-            this.itemHome.Text = "Home";
-            // 
-            // itemNew
-            // 
-            this.itemNew.Name = "itemNew";
-            this.itemNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.itemNew.Size = new System.Drawing.Size(152, 22);
-            this.itemNew.Text = "New";
-            this.itemNew.Click += new System.EventHandler(this.itemNew_Click);
-            // 
-            // itemSave
-            // 
-            this.itemSave.Name = "itemSave";
-            this.itemSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.itemSave.Size = new System.Drawing.Size(152, 22);
-            this.itemSave.Text = "Save";
-            // 
-            // itemExport
-            // 
-            this.itemExport.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.itemPlist,
-            this.itemJson});
-            this.itemExport.Name = "itemExport";
-            this.itemExport.Size = new System.Drawing.Size(152, 22);
-            this.itemExport.Text = "Export";
-            // 
-            // itemPlist
-            // 
-            this.itemPlist.Name = "itemPlist";
-            this.itemPlist.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
-            this.itemPlist.Size = new System.Drawing.Size(152, 22);
-            this.itemPlist.Text = "PLIST";
-            this.itemPlist.Click += new System.EventHandler(this.itemPlist_Click);
-            // 
-            // itemJson
-            // 
-            this.itemJson.Name = "itemJson";
-            this.itemJson.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.J)));
-            this.itemJson.Size = new System.Drawing.Size(152, 22);
-            this.itemJson.Text = "JSON";
-            // 
-            // itemExit
-            // 
-            this.itemExit.Name = "itemExit";
-            this.itemExit.Size = new System.Drawing.Size(152, 22);
-            this.itemExit.Text = "Exit";
-            // 
-            // itemAbout
-            // 
-            this.itemAbout.Name = "itemAbout";
-            this.itemAbout.Size = new System.Drawing.Size(52, 20);
-            this.itemAbout.Text = "About";
             // 
             // Id
             // 
@@ -149,6 +98,106 @@
             this.Vietnamese.HeaderText = "Vietnamese";
             this.Vietnamese.Name = "Vietnamese";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemHome,
+            this.itemEdit,
+            this.itemAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(851, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuMain";
+            // 
+            // itemHome
+            // 
+            this.itemHome.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemNew,
+            this.itemOpen,
+            this.itemSave,
+            this.itemExport,
+            this.itemExit});
+            this.itemHome.Name = "itemHome";
+            this.itemHome.Size = new System.Drawing.Size(62, 24);
+            this.itemHome.Text = "Home";
+            // 
+            // itemNew
+            // 
+            this.itemNew.Name = "itemNew";
+            this.itemNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.itemNew.Size = new System.Drawing.Size(167, 24);
+            this.itemNew.Text = "New";
+            this.itemNew.Click += new System.EventHandler(this.itemNew_Click);
+            // 
+            // itemOpen
+            // 
+            this.itemOpen.Name = "itemOpen";
+            this.itemOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.itemOpen.Size = new System.Drawing.Size(167, 24);
+            this.itemOpen.Text = "Open";
+            this.itemOpen.Click += new System.EventHandler(this.itemOpen_Click);
+            // 
+            // itemSave
+            // 
+            this.itemSave.Name = "itemSave";
+            this.itemSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.itemSave.Size = new System.Drawing.Size(167, 24);
+            this.itemSave.Text = "Save";
+            // 
+            // itemExport
+            // 
+            this.itemExport.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemPlist,
+            this.itemJson});
+            this.itemExport.Name = "itemExport";
+            this.itemExport.Size = new System.Drawing.Size(167, 24);
+            this.itemExport.Text = "Export";
+            // 
+            // itemPlist
+            // 
+            this.itemPlist.Name = "itemPlist";
+            this.itemPlist.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.itemPlist.Size = new System.Drawing.Size(163, 24);
+            this.itemPlist.Text = "PLIST";
+            this.itemPlist.Click += new System.EventHandler(this.itemPlist_Click);
+            // 
+            // itemJson
+            // 
+            this.itemJson.Name = "itemJson";
+            this.itemJson.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.J)));
+            this.itemJson.Size = new System.Drawing.Size(163, 24);
+            this.itemJson.Text = "JSON";
+            // 
+            // itemExit
+            // 
+            this.itemExit.Name = "itemExit";
+            this.itemExit.Size = new System.Drawing.Size(167, 24);
+            this.itemExit.Text = "Exit";
+            // 
+            // itemAbout
+            // 
+            this.itemAbout.Name = "itemAbout";
+            this.itemAbout.Size = new System.Drawing.Size(62, 24);
+            this.itemAbout.Text = "About";
+            this.itemAbout.Click += new System.EventHandler(this.itemAbout_Click);
+            // 
+            // itemEdit
+            // 
+            this.itemEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemColumns});
+            this.itemEdit.Name = "itemEdit";
+            this.itemEdit.Size = new System.Drawing.Size(47, 24);
+            this.itemEdit.Text = "Edit";
+            // 
+            // itemColumns
+            // 
+            this.itemColumns.Name = "itemColumns";
+            this.itemColumns.Size = new System.Drawing.Size(152, 24);
+            this.itemColumns.Text = "Columns";
+            this.itemColumns.Click += new System.EventHandler(this.itemColumns_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -159,6 +208,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Text Editor";
             ((System.ComponentModel.ISupportInitialize)(this.drgMain)).EndInit();
             this.menuStrip1.ResumeLayout(false);
@@ -180,9 +230,12 @@
         private System.Windows.Forms.ToolStripMenuItem itemJson;
         private System.Windows.Forms.ToolStripMenuItem itemExit;
         private System.Windows.Forms.ToolStripMenuItem itemAbout;
+        private System.Windows.Forms.ToolStripMenuItem itemOpen;
         private System.Windows.Forms.DataGridViewTextBoxColumn Id;
         private System.Windows.Forms.DataGridViewTextBoxColumn English;
         private System.Windows.Forms.DataGridViewTextBoxColumn Vietnamese;
+        private System.Windows.Forms.ToolStripMenuItem itemEdit;
+        private System.Windows.Forms.ToolStripMenuItem itemColumns;
     }
 }
 
